e34d138339246590fd30a86ce99671108f63bf8d
dieutuyetnguyenthi <dieutuyetnguyenthi@gmail.com>
2025-07-27 22:14:23 +0700

    done for reset password

--- src/main/java/org/yenln8/ChatApp/services/serviceImpl/auth/implement/ResetPasswordServiceImpl.java
+++ src/main/java/org/yenln8/ChatApp/services/serviceImpl/auth/implement/ResetPasswordServiceImpl.java
@@ -6,13 +6,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.yenln8.ChatApp.common.constant.EmailConstant;
+import org.yenln8.ChatApp.common.util.MessageBundle;
+import org.yenln8.ChatApp.common.util.Network;
 import org.yenln8.ChatApp.common.util.spam.SpamService;
 import org.yenln8.ChatApp.dto.base.BaseResponseDto;
 import org.yenln8.ChatApp.dto.request.ChangePasswordAccountRequestDto;
 import org.yenln8.ChatApp.dto.request.ResetPasswordAccountRequestDto;
-import org.yenln8.ChatApp.entity.User;
+import org.yenln8.ChatApp.entity.*;
 import org.yenln8.ChatApp.repository.*;
 import org.yenln8.ChatApp.services.serviceImpl.auth.interfaces.ResetPasswordService;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Slf4j
 @AllArgsConstructor
@@ -25,26 +32,93 @@ public class ResetPasswordServiceImpl implements ResetPasswordService {
     private PasswordEncoder passwordEncoder;
     private EmailOutboxRepository emailOutboxRepository;
     private SpamService spamService;
+    private RegisterServiceImpl registerService;
+    private PasswordPendingRepository passwordPendingRepository;
 
     @Override
     public BaseResponseDto call(ResetPasswordAccountRequestDto form, HttpServletRequest request) {
-//        User user = validate(loginRequestDto, request);
+        validate(form, request);
 
-//        String token = save(user,request);
+        save(form, request);
 
         return BaseResponseDto.builder()
                 .success(true)
                 .statusCode(HttpStatus.OK.value())
-                .message("OK")
+                .message(MessageBundle.getMessage("app.otp.registration.otp.sent"))
                 .build();
     }
 
-    private void validate(ChangePasswordAccountRequestDto form, HttpServletRequest request) {
+    private void validate(ResetPasswordAccountRequestDto form, HttpServletRequest request) {
+        String email = form.getEmail();
+        String newPassword = form.getNewPassword();
 
+        // kiem tra password moi co hop format khong
+        // - password it nhat 1 ki tu so
+        if (!registerService.containsDigit(newPassword))
+            throw new IllegalArgumentException(MessageBundle.getMessage("error.password.contains.digit"));
 
+        // + password it nhat 1 ki tu chu cai viet thuong
+        if (!registerService.containsLowercase(newPassword))
+            throw new IllegalArgumentException(MessageBundle.getMessage("error.password.contains.lowercase"));
+
+        // + password it nhat 1 ki tu chu cai viet hoa
+        if (!registerService.containsUppercase(newPassword))
+            throw new IllegalArgumentException(MessageBundle.getMessage("error.password.contains.uppercase"));
+
+        // + password it nhat 1 ki tu dac biet thuoc !@#$%^&*()_+-=[]{};\\':\"|,./<>?`~
+        if (!registerService.containsSpecialChar(newPassword))
+            throw new IllegalArgumentException(MessageBundle.getMessage("error.password.contains.character"));
+
+        // kiem tra co spam email k
+        Optional<BlackListSendEmail> blackListSendEmail = this.blackListSendEmailRepository.findByFromIpAddressAndToEmailAndType(Network.getUserIP(request), email, BlackListSendEmail.TYPE.FORGOT_PASSWORD);
+
+        if (blackListSendEmail.isPresent() &&
+                blackListSendEmail.get().getStatus().equals(BlackListSendEmail.STATUS.BAN) &&
+                blackListSendEmail.get().getFreeToSendAt().isAfter(LocalDateTime.now())
+        ) {
+            throw new IllegalArgumentException(MessageBundle.getMessage("error.email.exceed", EmailConstant.LIMIT_MINUTES_TO_SEND_EMAIL));
+        }
     }
 
-    private void save(User user, HttpServletRequest request) {
+    private void save(ResetPasswordAccountRequestDto form, HttpServletRequest request) {
+        String email = form.getEmail().trim();
+        String newPassword = form.getNewPassword().trim();
+        String ipAddress = Network.getUserIP(request);
+
+        // Luu thong tin OTP tao duoc vao bang OTP
+        OTP otp = this.OTPRepository.save(OTP.builder()
+                .type(OTP.TYPE.FORGOT_PASSWORD)
+                .otpCode(OTP.generateOTP())
+                .fromIpAddress(ipAddress)
+                .rowVersion(0)
+                .toEmail(email)
+                .expireAt(LocalDateTime.now().plusMinutes(EmailConstant.EXPIRE_TIME_IN_MINUTES))
+                .status(OTP.STATUS.BE_SENT)
+                .build());
+        log.info("otp saved: {}", otp);
+
+        User user = this.userRepository.findByEmailAndDeletedAtIsNull(form.getEmail()).orElseThrow();
+
+        // Luu thong tin vao bang Password Pending
+        PasswordPending passwordPending = this.passwordPendingRepository.save(PasswordPending.builder()
+                .otpId(otp.getId())
+                .newPassword(newPassword)
+                .user(user)
+                .type(PasswordPending.TYPE.FORGOT)
+                .status(PasswordPending.STATUS.PENDING)
+                .build());
+        log.info("passwordPending saved: {}", passwordPending);
+
+        // Kiem tra so lan gui email tu ip X toi email Y voi type = Z, neu dat threshhold, tien hanh cap nhat bang BlackList de cam gui email tiep
+        this.spamService.avoidSpamOTP(ipAddress, email, BlackListSendEmail.TYPE.FORGOT_PASSWORD);
+
+        // Gui email
+        EmailOutbox emailOutboxSaved = this.emailOutboxRepository.save(EmailOutbox.builder()
+                .toEmail(email)
+                .otpCode(otp.getOtpCode())
+                .type(EmailOutbox.TYPE.FORGOT_PASSWORD)
+                .build());
+        log.info("EmailOutbox saved: {}", emailOutboxSaved);
 
     }
 }
--- src/main/java/org/yenln8/ChatApp/services/serviceImpl/auth/implement/VerifyResetPasswordServiceImpl.java
+++ src/main/java/org/yenln8/ChatApp/services/serviceImpl/auth/implement/VerifyResetPasswordServiceImpl.java
@@ -6,44 +6,93 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.yenln8.ChatApp.common.util.spam.SpamService;
+import org.yenln8.ChatApp.common.util.MessageBundle;
 import org.yenln8.ChatApp.dto.base.BaseResponseDto;
 import org.yenln8.ChatApp.dto.request.VerifyOtpRequestDto;
+import org.yenln8.ChatApp.entity.OTP;
+import org.yenln8.ChatApp.entity.PasswordPending;
 import org.yenln8.ChatApp.entity.User;
-import org.yenln8.ChatApp.repository.*;
+import org.yenln8.ChatApp.repository.OTPRepository;
+import org.yenln8.ChatApp.repository.PasswordPendingRepository;
+import org.yenln8.ChatApp.repository.UserRepository;
+import org.yenln8.ChatApp.services.serviceImpl.auth.interfaces.LogOutService;
 import org.yenln8.ChatApp.services.serviceImpl.auth.interfaces.VerifyResetPasswordService;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Slf4j
 @AllArgsConstructor
 @Service
 public class VerifyResetPasswordServiceImpl implements VerifyResetPasswordService {
     private UserRepository userRepository;
-    private OTPRepository OTPRepository;
-    private BlackListSendEmailRepository blackListSendEmailRepository;
-    private AccountPendingRepository accountPendingRepository;
+    private OTPRepository otpRepository;
+    private PasswordPendingRepository passwordPendingRepository;
     private PasswordEncoder passwordEncoder;
-    private EmailOutboxRepository emailOutboxRepository;
-    private SpamService spamService;
+    private LogOutService logOutService;
 
     @Override
     public BaseResponseDto call(VerifyOtpRequestDto form, HttpServletRequest request) {
-//        User user = validate(loginRequestDto, request);
+        OTP otp = validate(form, request);
 
-//        String token = save(user,request);
+        save(form, otp, request);
 
         return BaseResponseDto.builder()
                 .success(true)
                 .statusCode(HttpStatus.OK.value())
-                .message("OK")
+                .message(MessageBundle.getMessage("app.email.reset.password.success"))
                 .build();
     }
 
-    private void validate(VerifyOtpRequestDto form, HttpServletRequest request) {
+    private OTP validate(VerifyOtpRequestDto form, HttpServletRequest request) {
+        // Tim ban ghi ung voi OTP trong bang OTP va type REGISTER_ACCOUNT, neu khong ton tai hoac ton tai nhung het han, nem loi
+        String otpCode = form.getOtp();
 
+        Optional<OTP> optionalOTP = this.otpRepository.findByOtpCodeAndTypeAndDeletedAtIsNull(otpCode, OTP.TYPE.FORGOT_PASSWORD);
 
+        // Validate exist
+        if (optionalOTP.isEmpty()) throw new IllegalArgumentException(MessageBundle.getMessage("error.otp.incorrect"));
+
+        OTP otp = optionalOTP.get();
+        log.info("OTP code {} found", otp);
+
+        // Validate expired
+        if (otp.getExpireAt().isBefore(LocalDateTime.now()))
+            throw new IllegalArgumentException(MessageBundle.getMessage("error.otp.expire"));
+
+        return otp;
     }
 
-    private void save(User user, HttpServletRequest request) {
+    private void save(VerifyOtpRequestDto form, OTP otp, HttpServletRequest request) {
+        // Cap nhat OTP tu trang thai BE_SENT thanh VERIFIED va deletedAt = now()  + deleted = id
+        otp.setStatus(OTP.STATUS.VERIFIED);
+        otp.setDeletedAt(LocalDateTime.now());
+        otp.setDeleted(otp.getId());
 
+        this.otpRepository.save(otp);
+
+        // Cap nhat PasswordPending
+        Optional<PasswordPending> optionalPasswordPending = this.passwordPendingRepository.findByOtpIdAndStatusAndDeletedAtIsNull(otp.getId(), PasswordPending.STATUS.PENDING);
+        log.info("Password Pending: {}", optionalPasswordPending);
+
+        if (optionalPasswordPending.isEmpty())
+            throw new IllegalArgumentException(MessageBundle.getMessage("error.object.not.found", "PasswordPending", "otpId", otp.getId()));
+
+        PasswordPending passwordPending = optionalPasswordPending.get();
+        passwordPending.setStatus(PasswordPending.STATUS.DONE);
+        passwordPending.setDeletedAt(LocalDateTime.now());
+        passwordPending.setDeleted(passwordPending.getId());
+        this.passwordPendingRepository.save(passwordPending);
+
+        // Update password
+
+        User user = this.userRepository.findById(passwordPending.getUser().getId()).orElseThrow(() -> new IllegalArgumentException(MessageBundle.getMessage("error.object.not.found", "PasswordPending", "otpId", otp.getId())));
+        user.setPassword(passwordEncoder.encode(passwordPending.getNewPassword()));
+
+        this.userRepository.save(user);
+        log.info("new User : {}", user);
+
+        // logout all device
+        this.logOutService.logOutAllDevice(user.getId());
     }
 }

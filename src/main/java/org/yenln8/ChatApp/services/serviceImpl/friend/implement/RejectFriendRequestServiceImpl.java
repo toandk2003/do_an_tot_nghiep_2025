a5bfca3ec266eae1d05a5a441055002175bc97e4
dieutuyetnguyenthi <dieutuyetnguyenthi@gmail.com>
2025-11-09 22:38:35 +0700

    emit reject FR

--- src/main/java/org/yenln8/ChatApp/services/serviceImpl/friend/implement/RejectFriendRequestServiceImpl.java
+++ src/main/java/org/yenln8/ChatApp/services/serviceImpl/friend/implement/RejectFriendRequestServiceImpl.java
@@ -1,16 +1,21 @@
 package org.yenln8.ChatApp.services.serviceImpl.friend.implement;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.yenln8.ChatApp.common.util.ContextService;
 import org.yenln8.ChatApp.common.util.MessageBundle;
 import org.yenln8.ChatApp.dto.base.BaseResponseDto;
 import org.yenln8.ChatApp.dto.other.CurrentUser;
 import org.yenln8.ChatApp.dto.response.RejectFriendResponseDto;
+import org.yenln8.ChatApp.entity.Event;
 import org.yenln8.ChatApp.entity.FriendRequest;
 import org.yenln8.ChatApp.entity.User;
+import org.yenln8.ChatApp.repository.EventRepository;
 import org.yenln8.ChatApp.repository.FriendRequestRepository;
+import org.yenln8.ChatApp.repository.UserRepository;
 import org.yenln8.ChatApp.services.serviceImpl.friend.interfaces.RejectFriendRequestService;
 import org.yenln8.ChatApp.services.serviceImpl.user.interfaces.GetFullInfoAboutUserService;
 
@@ -22,6 +27,10 @@ import java.time.LocalDateTime;
 public class RejectFriendRequestServiceImpl implements RejectFriendRequestService {
     private FriendRequestRepository friendRequestRepository;
     private GetFullInfoAboutUserService getFullInfoAboutUserService;
+    @Autowired
+    private EventRepository eventRepository;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Override
     public BaseResponseDto call(Long friendRequestId) {
@@ -44,12 +53,27 @@ public class RejectFriendRequestServiceImpl implements RejectFriendRequestServic
                 .responseAt(friendRequestSaved.getResponsedAt())
                 .build();
 
-        return BaseResponseDto.builder()
+        var response = BaseResponseDto.builder()
                 .success(true)
                 .statusCode(200)
                 .data(responseDto)
                 .message("Reject friend request successfully")
+                .eventType(Event.TYPE.REJECT_FRIEND_REQUEST)
                 .build();
+
+        try {
+            eventRepository.save(Event.builder()
+                    .payload(objectMapper.writeValueAsString(response))
+                    .destination("sync-stream")
+                    .status(Event.STATUS.WAIT_TO_SEND)
+                    .createdAt(LocalDateTime.now())
+                    .updatedAt(LocalDateTime.now())
+                    .build());
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        }
+
+        return response;
     }
 
     private FriendRequest save(FriendRequest friendRequest) {

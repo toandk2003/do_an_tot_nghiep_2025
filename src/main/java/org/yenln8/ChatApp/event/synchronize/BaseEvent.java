c27dc138fd894c5f95d5ec62d611a53c2b9933c7
dieutuyetnguyenthi <dieutuyetnguyenthi@gmail.com>
2025-11-15 22:02:25 +0700

    Add checkDuplicate

--- src/main/java/org/yenln8/ChatApp/event/synchronize/BaseEvent.java
+++ src/main/java/org/yenln8/ChatApp/event/synchronize/BaseEvent.java
@@ -1,14 +1,18 @@
 package org.yenln8.ChatApp.event.synchronize;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 import org.yenln8.ChatApp.entity.Event;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 @SuperBuilder
 @Data
 public class BaseEvent implements Serializable {
     private Event.TYPE eventType ;
     private Integer rowVersion;
+    @Builder.Default
+    private String checkDuplicate = UUID.randomUUID().toString();
 }
